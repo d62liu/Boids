532d8b0423c60d7374f14189d57ef3ec9280cc5d
Daniel liu <liudaniel0322@gmail.com>
2025-01-15 23:08:28 -0500

    Cohesion, Alignment and Separation Functions

--- boid_main.cpp
+++ boid_main.cpp
@@ -41,6 +41,8 @@ struct Vec2 {
 struct Boid {
     Vec2 pos;
     Vec2 vel;
+    double view_radius, protected_radius;
+    double avoid_factor, matching_factor, centering_factor, turn_factor;
     Boid(Vec2 pos) {
         this->pos = pos;
         this->vel = Vec2();
@@ -55,7 +57,7 @@ class Scene {
 private:
     std::vector<Boid> birds; //naive vector
     size_t height, width;
-    double param1, param2, param3; //parameters for boids (rename them)
+     //parameters for boids 
 public:
     Scene(int, int) {
         height = 0; width = 0; //etc
@@ -63,19 +65,37 @@ public:
     double getParam1() {}; //...etc
     double setParam1() {}; //...etc
 
-    void update(void) {
+    void update(double const dt) {
         size_t const sz = birds.size();
         for(size_t i = 0; i < sz; ++i) {
-            for(size_t j = 0; < sz; ++j) {
+            Vec2 p_tot = Vec2(); //Cohesion
+            int neighbouring_boids = 0;
+            Vec2 distance = Vec2();//Separation
+            Vec2 v_tot = Vec2(); //Alignment
+            for(size_t j = 0; j < sz; ++j) {
                 Vec2 p1 = birds[i].pos;
                 Vec2 v1 = birds[i].vel;
                 Vec2 p2 = birds[j].pos;
                 Vec2 v2 = birds[j].vel;
-                if(((v1 + v2)/(v1+v2).mag()).x == 69); //domath
+                if(((v1-v2).mag()) <= birds[i].view_radius){ //setting variables for Cohesion and Alignment
+                    p_tot = p_tot + birds[i].pos;
+                    v_tot = v_tot + birds[i].vel;
+                    neighbouring_boids = neighbouring_boids + 1;
+                if(((v1-v2).mag()) <= birds[i].protected_radius){ //setting variables for Separation
+                    distance = distance + (birds[i].pos - birds[j].pos);
+                }
+            birds[i].pos = birds[i].pos + distance * birds[i].avoid_factor; //calculation for Separation
+            if (neighbouring_boids > 0){ //Calculation for Cohesion and Alignment
+                Vec2 const p_avg = p_tot/neighbouring_boids;
+                Vec2 const v_avg = v_tot/neighbouring_boids;
+                birds[i].vel = birds[i].vel + (p_avg - birds[i].pos) * birds[i].centering_factor;
+                birds[i].vel = birds[i].vel + (v_avg - birds[i].vel) * birds[i].matching_factor;
 
             }
+                }    
         }
     }
 
 };
+}
 
