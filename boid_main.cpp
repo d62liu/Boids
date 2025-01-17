e718266ca4646a3387858035972cf4bfb5b3f0c9
Daniel liu <liudaniel0322@gmail.com>
2025-01-17 16:26:42 -0500

    Update boid_main.cpp

--- boid_main.cpp
+++ boid_main.cpp
@@ -16,8 +16,8 @@ struct Vec2 {
         this->x = x;
         this->y = y;
     }
-    double mag(void) {
-        return (0.96043387*std::max(x,y) + 0.397824734*std::min(x,y));
+    double mag(void) { 
+        return (sqrt(x*x + y*y));//(0.96043387*std::max(x,y) + 0.397824734*std::min(x,y));
     }
     Vec2 operator+(Vec2 const& v) const {
         return Vec2(x+v.x, y+v.y);
@@ -58,11 +58,13 @@ class Scene {
 private:
     std::vector<Boid> birds; //naive vector
     int height, width;
-    int const bottom_border, top_border, left_border, right_border;
+    Vec2 const initial_vel; 
      //parameters for boids 
 public:
-    Scene(int, int) {
-        height = 0; width = 0; //etc
+    Scene(int h, int w, Vec2 const& init_vel) {
+        height = h; 
+        width = w; //etc
+        Vec2 initial_vel = init_vel;
     }
     double getParam1() {}; //...etc
     double setParam1() {}; //...etc
@@ -70,6 +72,11 @@ public:
         return birds;
     }
     void update(double const dt) {
+        int const bottom_border = height + 1;
+        int const top_border = height - 1;
+        int const right_border = width + 1;
+        int const left_border = width - 1;
+
         size_t const sz = birds.size();
         for(size_t i = 0; i < sz; ++i) {
             Vec2 p_tot = Vec2(); //Cohesion
@@ -77,41 +84,45 @@ public:
             Vec2 distance = Vec2();//Separation
             Vec2 v_tot = Vec2(); //Alignment
             for(size_t j = 0; j < sz; ++j) {
-                //when calc conditions then use these, when modifying, use birds[i]
-                //NOTE: birds[i] is expensive
                 Vec2 const p1 = birds[i].pos;
                 Vec2 const v1 = birds[i].vel;
                 Vec2 const p2 = birds[j].pos;
                 Vec2 const v2 = birds[j].vel;
-                if(((v1-v2).mag()) <= birds[i].view_radius){ //setting variables for Cohesion and Alignment
-                    p_tot = p_tot + birds[i].pos;
-                    v_tot = v_tot + birds[i].vel;
+                const double turn_accel = birds[i].turn_factor;
+                if(((p1-p2).mag()) <= birds[i].view_radius){ //setting variables for Cohesion and Alignment
+                    p_tot = p_tot + p2;
+                    v_tot = v_tot + v2;
                     neighbouring_boids = neighbouring_boids + 1;
                 }
-                if(((v1-v2).mag()) <= birds[i].protected_radius){ //setting variables for Separation
-                    distance = distance + (birds[i].pos - birds[j].pos);
-                }
-            birds[i].pos = birds[i].pos + distance * birds[i].avoid_factor; //calculation for Separation
+
+                if(((p1-p2).mag()) <= birds[i].protected_radius)distance = distance + (p1 - p2);  //setting variables for Separation
+
+            birds[i].pos = p1 + distance * birds[i].avoid_factor; //calculation for Separation
+
             if (neighbouring_boids > 0){ //Calculation for Cohesion and Alignment
                 Vec2 const p_avg = p_tot/neighbouring_boids;
                 Vec2 const v_avg = v_tot/neighbouring_boids;
-                birds[i].vel = birds[i].vel + (p_avg - birds[i].pos) * birds[i].centering_factor;
-                birds[i].vel = birds[i].vel + (v_avg - birds[i].vel) * birds[i].matching_factor;
+                birds[i].vel = v1 + (p_avg - p1) * birds[i].centering_factor;
+                birds[i].vel = v1 + (v_avg - v1) * birds[i].matching_factor;
 
-            }if (birds[i].pos.x < left_border){ //turning from border
-                birds[i].vel.x = birds[i].vel.x + birds[i].turn_factor;
-            }if (birds[i].pos.x > right_border){
-                birds[i].vel.x = birds[i].vel.x - birds[i].turn_factor;
-            }if (birds[i].pos.y > top_border){
-                birds[i].vel.y = birds[i].vel.y - birds[i].turn_factor;
-            }if (birds[i].pos.y < bottom_border){
-                birds[i].vel.y = birds[i].vel.y + birds[i].turn_factor;
             }
-            double speed = birds[i].vel.mag(); 
-            if (speed < birds[i].min_speed) birds[i].vel = birds[i].vel * birds[i].mix_speed/speed;
-            if (speed > birds[i].max_speed) birds[i].vel = birds[i].vel * birds[i].max_speed/speed;
+            if (p1.x < left_border) birds[i].vel.x = v1.x + turn_accel*dt;
+            if (p1.x > right_border)birds[i].vel.x = v1.x - turn_accel*dt;
+            if (p1.y > top_border)birds[i].vel.y = v1.y - turn_accel*dt;
+            if (p1.y < bottom_border)birds[i].vel.y = v1.y + turn_accel*dt;
 
+            double speed = birds[i].vel.mag(); 
+
+            if (speed < birds[i].min_speed) birds[i].vel = v1 * birds[i].min_speed/speed;
+            if (speed > birds[i].max_speed) birds[i].vel = v1 * birds[i].max_speed/speed;
+            
         }
     }
+    for(size_t i = 0; i < sz; ++i) {
+        birds[i].pos = birds[i].pos + birds[i].vel * dt;
+    }
+    }
+    void makeBoid(double, double){
+        birds.push_back(Boid(initial_vel, Vec2(0,0))); //intiialize at 0,0
     }
 };
